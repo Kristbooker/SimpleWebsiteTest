d3c90b355737d5b393ca1f7cdb5411fb99301fe7
kristbooker <boybook123@gmail.com>
2023-11-16 01:04:08 +0700

    second commit

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ function checkPassword() {
     var password = document.getElementById('passwordInput').value.toLowerCase();
 
     // เปลี่ยนรหัสผ่านตามที่คุณต้องการ
-    if (password === 'islam' || password === 'อิสลาม') {
+    if (password === '24' || password === 'ยี่สิบสี่') {
         document.getElementById('imageContainer').innerHTML = '<img src="qrcode_wy.png" alt="รูปภาพ">';
         document.getElementById('imageContainer').style.display = 'block';
     } else {
-        alert('รหัสผ่านไม่ถูกต้อง');
+        alert('คำตอบไม่ถูกต้อง');
     }
 }
